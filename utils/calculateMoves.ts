a59bdd999e5254624617a7f9183a961dc8f55722
Douglas <dogge20@hotmail.com>
2022-05-02 20:02:30 +0200

    horse king queen moves

--- utils/calculateMoves.ts
+++ utils/calculateMoves.ts
@@ -15,6 +15,12 @@ const getMoves = (piece: Piece, board: any, userColor: string) => {
       return getRookMoves(piece, board, userColor)
     case 'bishop':
       return getBishopMoves(piece, board, userColor)
+    case 'king':
+      return getKingMoves(piece, board, userColor)
+    case 'knight':
+      return getKnightMoves(piece, board, userColor)
+    case 'queen':
+      return [...getBishopMoves(piece, board, userColor), ...getRookMoves(piece, board, userColor)]
     default:
       return []
     // code block
@@ -85,4 +91,62 @@ const getBishopMoves = (piece: Piece, board: Tile[], userColor: string) => {
   return moves
 }
 
+const getKnightMoves = (piece: Piece, board: Tile[], userColor: string) => {
+  const moves = []
+  moves.push({ row: piece.row + 2, col: piece.col + 1 })
+  moves.push({ row: piece.row + 2, col: piece.col - 1 })
+  moves.push({ row: piece.row - 2, col: piece.col + 1 })
+  moves.push({ row: piece.row - 2, col: piece.col - 1 })
+  moves.push({ row: piece.row - 1, col: piece.col - 2 })
+  moves.push({ row: piece.row - 1, col: piece.col + 2 })
+  moves.push({ row: piece.row + 1, col: piece.col + 2 })
+  moves.push({ row: piece.row + 1, col: piece.col - 2 })
+
+  return moves
+}
+
+const getKingMoves = (piece: Piece, board: any, userColor: string) => {
+  const moves = []
+  for (let i = 1; i < 2; i++) {
+    if (piece.row + i < 8 && existsNoPieceOnTile(piece.row + i, piece.col + i, board)) {
+      moves.push({ row: piece.row + i, col: piece.col + i })
+    } else break
+  }
+  for (let i = 1; i < 2; i++) {
+    if (piece.row - i >= 0 && existsNoPieceOnTile(piece.row - i, piece.col - i, board)) {
+      moves.push({ row: piece.row - i, col: piece.col - i })
+    } else break
+  }
+  for (let i = 1; i < 2; i++) {
+    if (piece.row + i < 8 && existsNoPieceOnTile(piece.row + i, piece.col - i, board)) {
+      moves.push({ row: piece.row + i, col: piece.col - i })
+    } else break
+  }
+  for (let i = 1; i < 2; i++) {
+    if (piece.row - i >= 0 && existsNoPieceOnTile(piece.row - i, piece.col + i, board)) {
+      moves.push({ row: piece.row - i, col: piece.col + i })
+    } else break
+  }
+  for (let i = 1; i < 2; i++) {
+    if (piece.row + i < 8 && existsNoPieceOnTile(piece.row + i, piece.col, board)) {
+      moves.push({ row: piece.row + i, col: piece.col })
+    } else break
+  }
+  for (let i = 1; i < 2; i++) {
+    if (piece.row - i >= 0 && existsNoPieceOnTile(piece.row - i, piece.col, board)) {
+      moves.push({ row: piece.row - i, col: piece.col })
+    } else break
+  }
+  for (let i = 1; i < 2; i++) {
+    if (piece.col - i >= 0 && existsNoPieceOnTile(piece.row, piece.col - i, board)) {
+      moves.push({ row: piece.row, col: piece.col - i })
+    } else break
+  }
+  for (let i = 1; i < 2; i++) {
+    if (piece.col + i < 8 && existsNoPieceOnTile(piece.row, piece.col + i, board)) {
+      moves.push({ row: piece.row, col: piece.col + i })
+    } else break
+  }
+  return moves
+}
 export default getMoves
