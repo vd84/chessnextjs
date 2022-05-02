ff6fae401095a74b2622d404ad66747c54fd50a4
Douglas <dogge20@hotmail.com>
2022-05-02 16:49:39 +0200

    rook can move straight lines (also jump over)

--- utils/calculateMoves.ts
+++ utils/calculateMoves.ts
@@ -4,6 +4,8 @@ const getMoves = (piece: Piece, board: any, userColor: string) => {
   switch (piece.pieceType) {
     case 'pawn':
       return getPawnMoves(piece, board, userColor)
+    case 'rook':
+      return getRookMoves(piece, board, userColor)
     default:
       return []
     // code block
@@ -21,6 +23,18 @@ const getPawnMoves = (piece: Piece, board: any, userColor: string) => {
   if (board[piece.id - 1].piece?.pieceType === 'pawn' && board[piece.id + 1].piece?.amountMoves === 1) {
     moves.push({ row: piece.row - 1, col: piece.col - 1 })
   }
+  return moves
+}
+
+const getRookMoves = (piece: Piece, board: any, userColor: string) => {
+  const moves = []
+  for (let i = 0; i < 8; i++) {
+    moves.push({ row: piece.row + i, col: piece.col })
+    moves.push({ row: piece.row - i, col: piece.col })
+    moves.push({ row: piece.row, col: piece.col + i })
+    moves.push({ row: piece.row, col: piece.col - i })
+  }
+
   return moves
 }
 
